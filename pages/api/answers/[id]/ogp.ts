e170de0f4bec03e52a54ddfb5c08a99461b2b74a
hatsu38 <hajiwata0308@gmail.com>
2021-01-09 20:19:12 +0900

    Create OGP Generator

--- pages/api/answers/[id]/ogp.ts
+++ pages/api/answers/[id]/ogp.ts
@@ -1,8 +1,58 @@
-import * as path from 'path'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas, loadImage } from 'canvas'
+import { createCanvas, registerFont, loadImage } from 'canvas'
+import * as path from 'path'
+import '../../../../lib/firebase_admin'
+import { firestore } from 'firebase-admin'
+import { Answer } from '../../../../models/Answer'
+import { Question } from '../../../../models/Question'
+
+type SeparatedText = {
+  line: string
+  remaining: string
+}
+function createTextLine(context, text: string): SeparatedText {
+  const maxWidth = 400
+
+  for (let i = 0; i < text.length; i++) {
+    const line = text.substring(0, i + 1)
+    if (context.measureText(line).width > maxWidth) {
+      return {
+        line,
+        remaining: text.substring(i + 1),
+      }
+    }
+  }
+
+  return {
+    line: text,
+    remaining: '',
+  }
+}
+
+function createTextLines(context, text: string): string[] {
+  const lines: string[] = []
+  let currentText = text
+
+  while (currentText !== '') {
+    const separatedText = createTextLine(context, currentText)
+    lines.push(separatedText.line)
+    currentText = separatedText.remaining
+  }
+
+  return lines
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id as string
+
+  const answerDoc = await firestore().collection('answers').doc(id).get()
+  const answer = answerDoc.data() as Answer
+  const questionDoc = await firestore()
+    .collection('questions')
+    .doc(answer.questionId)
+    .get()
+  const question = questionDoc.data() as Question
+
   const width = 600
   const height = 315
   const canvas = createCanvas(width, height)
@@ -19,7 +69,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   context.font = '30px ipagp'
   context.textAlign = 'center'
   context.textBaseline = 'middle'
-  context.fillText('testテスト', 100, 50)
+  const lines = createTextLines(context, question.body)
+  lines.forEach((line, index) => {
+    const y = 157 + 40 * (index - (lines.length - 1) / 2)
+    context.fillText(line, 300, y)
+  })
 
 
   const buffer = canvas.toBuffer()
